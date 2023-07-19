9c3d2fb69c8cdbdc0b06e6497fc4eb8a1cd31838
Farid <128354927+cd043136@users.noreply.github.com>
2023-07-19 16:00:28 +0800

    fix bossalert triggering on failed

--- features/slayer/bossalert.js
+++ features/slayer/bossalert.js
@@ -4,29 +4,38 @@ import { slayerLocationCheck, slayerFightCheck } from "../../utils/slayer";
 import { registerWhen } from "../../utils/triggers";
 
 let bossSpawned = false
+let failed = false
 let stopRinging = true
-let n = 1  // cycle between 0 1
+let n = 1
 
 registerWhen(register("tick", () => {
+    // sound
+    if (!stopRinging) {
+        n = ((n + 1) % 2)
+        World.playSound("note.pling", 1, n)
+    }
+
+    // boss spawns
     if (slayerFightCheck() && !bossSpawned) {
         // alert
         bossSpawned = true
         stopRinging = false
         n = 1
-        setTimeout(() => { stopRinging = true }, 500)
+        setTimeout(() => { stopRinging = true }, 750)
         bossAlert()
         return
     }
+
+    // boss dies
     if (!slayerFightCheck() && bossSpawned) {
         bossSpawned = false
-        bossDied()
-    }
 
-    if (!stopRinging) {
-        n = (n + 1) % 2
-        World.playSound("note.pling", 1, n)
+        setTimeout(() => {
+            if (failed) return failed = false
+            else bossDied()
+        }, 250)
     }
-}), () => slayerLocationCheck())
+}), () => slayerLocationCheck() && settings.bossAlert)
 
 const bossAlert = () => {
     Client.showTitle(`${Format.BOLD}${Colour.RED}BOSS SPAWNED!`, "", 0, 20, 2)
@@ -35,3 +44,7 @@ const bossAlert = () => {
 const bossDied = () => {
     Client.showTitle(`${Format.BOLD}${Colour.GREEN}BOSS SLAIN!`, "", 0, 20, 2)
 }
+
+register("chat", (a, b, c) => {
+    if (b === "FAILED") failed = true
+}).setCriteria(/(  SLAYER QUEST (FAILED|STARTED)!)|(Your Slayer Quest has been cancelled!)/)
